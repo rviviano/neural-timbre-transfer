529bff2fe8626ab3a461324b81f418ae88a97602
Raymond Viviano <rayviviano@gmail.com>
2020-08-20 13:55:10 -0400

    Add functs to process/check options
    Make sure that users supply valid input wavs and a directory to write output to.

--- neural_timbre_transfer.py
+++ neural_timbre_transfer.py
@@ -47,6 +47,95 @@ def deprecated(func):
 
 
 # Function definitions
+def process_options():
+    """
+        Process command line arguments for file inputs and output. 
+        Also provide a help option. If specific options that could
+        usefully influence the style transfer come up, implement them.
+    """
+
+    # Define usage
+    usage = """
+
+    Usage: python mudpie_sample_generator --in <arg> --out <arg> --pre <arg> 
+                                          --num <arg> -h
+
+    Mandatory Options:
+
+        --c, --content  The wav file to take the content from.
+        
+        --s, --style    The wav file to take the style (timbre/spectral 
+                        characteristics) from to transfer to the content.
+
+        --o, --out      Directory to save output wavs to. If the provided path 
+                        does not exist, then the script will try to create it.
+
+    Optional Options:
+
+        -h             Print this usage message and exit
+
+    """
+
+    # Get commandline options and arguments
+    opts, _ = getopt.getopt(sys.argv[1:], "h:", ["content=", "style=", "out="])
+
+    # Set variables to defaults
+    content_path, style_path, out_dir = None, None, None
+
+    for opt, arg in opts:
+        # Mandatory arguments
+        if opt == "--c" or opt == "--content":
+            if (arg is not None) and (isfile(abspath(arg))):
+                content_path = check_input_arg(arg)
+
+        if opt == "--s" or opt == "--style":
+            if (arg is not None) and (isfile(abspath(arg))):
+                style_path = check_input_arg(arg)
+
+        if opt == "--o" or opt == "--out": 
+            if arg is not None:
+                out_dir = arg
+                # If the specified out dir does not exist, try to make it
+                if not isdir(abspath(out_dir)): 
+                    try:
+                        os.makedirs(abspath(out_dir))
+                    except:
+                        traceback.print_exc(file=sys.stdout)
+                        print(os.linesep + 'Cannot create output directory.')
+                        sys.exit(1)
+
+        # Optional options
+        if opt == "-h":
+            print(usage)
+            sys.exit(0)
+
+
+    # Make sure that arguments exist for all mandatory options
+    if None in [content_path, style_path, out_dir]:
+        print(os.linesep + 'Errors detected with mandatory options')
+        print(usage)
+        sys.exit(1)
+
+    # Return options for audio processing
+    return content_path, style_path, out_dir
+
+
+def check_input_arg(arg):
+    """ Check that file supplied on the command line is a 16 or 24 bit wav """
+    try:
+        wv_hdr = sndhdr.what(abspath(arg))
+        if wv_hdr is not None:
+            input_path = arg
+            return input_path
+        else: 
+            raise WavError("You must supply a 16- or 24-bit int wav")
+    except WavError:
+        raise
+    except:
+        traceback.print_exc(file=sys.stdout)
+        print(os.linesep + 'Unexpected input error.')
+        sys.exit(1)
+
 
 def load_wav():
     pass
